37b20179d001bd17c39039a576edd250fd95655c
Priya Hiremath <priyahrish18@gmail.com>
2018-06-29 17:45:40 +1200

    Add the getplayer utility function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import getPlayer from './getPlayer'
+
+export { getPlayer }
 
 import { App } from './components'
 import registerServiceWorker from './registerServiceWorker'
