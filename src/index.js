5dc79fc40b4794c71884a73519351620a3b8b949
Priya Hiremath <priyahrish18@gmail.com>
2018-06-29 20:44:50 +1200

    Add click handler to squers,update snapshots

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import getPlayer from './getPlayer'
-
-export { getPlayer }
 
 import { App } from './components'
 import registerServiceWorker from './registerServiceWorker'
